2c1d8567b8826c324ffa25190ac6b448a46b8298
Aditya Shukla <Shadowshukla76@gmail.com>
2025-06-24 11:59:29 +0530

    Fixed Env Issues

--- src/config/envs.js
+++ src/config/envs.js
@@ -1,5 +1,5 @@
 import { configDotenv } from "dotenv";
 configDotenv();
 export const PORT = process.env.PORT || 3000;
-export const mongodb_uri = process.env.MONGODB_URI;
+export const mongodb_uri = process.env.MONGODB_URL;
 export const DEFAULT_PROFILE_PIC = process.env.DEFAULT_PROFILE_PIC;
