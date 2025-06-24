6226b7b1e4ced60308a991db8fe6fea168346995
Aditya Shukla <Shadowshukla76@gmail.com>
2025-06-24 12:09:23 +0530

    fixed custom error handler

--- src/middlewares/CustomErrorHandler.js
+++ src/middlewares/CustomErrorHandler.js
@@ -1,14 +1,14 @@
 import { logger } from "../config/loggerConfig.js";
 
-export const CustomErrorHandler = async (error, req, res, next) => {
-  error.statusCode = error.statusCode || 500;
-  error.status = error.status || "Error";
+export const CustomErrorHandler = async (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "Error";
   logger.error(
-    `Failed to get the error the status code is ${err.status} and the message is ${err.message}`
+    `Failed to get the err the status code is ${err.status} and the message is ${err.message}`
   );
-  res.status(error.statusCode).json({
-    status: error.statusCode,
-    message: error.message,
+  res.status(err.statusCode).json({
+    status: err.statusCode,
+    message: err.message,
     success: false,
   });
 };
