4c1903d9ab5dbd20ed0ed33e1429871f553a2dd0
Aditya Shukla <Shadowshukla76@gmail.com>
2025-06-24 14:37:47 +0530

    formatted the code

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,7 +1,12 @@
 import express from "express";
 import { validateUserSchema } from "../validators/UserValidator.js";
 import { UserSchema } from "../models/User.js";
-import { DeleteUser, GetSingleUser, GetUsers, SignupUser } from "../controllers/userController.js";
+import {
+  DeleteUser,
+  GetSingleUser,
+  GetUsers,
+  SignupUser,
+} from "../controllers/userController.js";
 import { CheckAuth } from "../middlewares/CheckAuth.js";
 
 export const UserRouter = express.Router();
@@ -10,6 +15,4 @@ UserRouter.post("/signup", validateUserSchema(UserSchema), SignupUser);
 UserRouter.get("/user/:userId", GetSingleUser);
 UserRouter.get("/", GetUsers);
 
-UserRouter.delete("/", CheckAuth,DeleteUser);
-
-
+UserRouter.delete("/", CheckAuth, DeleteUser);
